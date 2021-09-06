6ec6f96205683078721a8cbf1648774c17356545
efbonneville <e.f.bonneville@lumc.nl>
2021-09-06 16:09:11 +0200

    Removed clutter .R files, on simulating with tdep DLI effect (can recover later)

--- _targets.R
+++ _targets.R
@@ -25,6 +25,7 @@ project_pkgs <- c(
 tar_option_set(packages = project_pkgs)
 # Uncomment if running scripts interactively:
 # sapply(project_pkgs, function(pkg) require(pkg, character.only = TRUE))
+rm(project_pkgs)
 
 
 # Miscellaneous objects ---------------------------------------------------
@@ -85,17 +86,6 @@ list(
         strata(trans)
     )
   ),
-  tar_target(
-    cox_dli_gvhd,
-    run_cox_submodel(
-      dli_msdata = dli_msdata,
-      form = Surv(Tstart, Tstop, status) ~
-        hirisk.1 + ATG.1 + # Relapse submodel
-        DLI.2 + ATG.2 + # GVHD submodel
-        ATG.3 + # NRF_other submodel
-        strata(trans)
-    )
-  ),
 
   # Part 3a: Run univariate joint models with both packages
   tar_target(
@@ -107,7 +97,7 @@ list(
     )
   ),
   tar_target(
-    JM_CD4_allDLI_inter,
+    JM_CD4_allDLI_inter, # this we keep for illustration
     run_jointModel(
       long_obj = long_submodels$CD4_abs_log,
       surv_obj = cox_all_dli,
@@ -124,32 +114,6 @@ list(
         "CD4_abs_log" = ~ value(CD4_abs_log):(trans1 + trans2 + trans3 - 1)
       ),
       data_Surv = dli_msdata,
-      n_iter = 15000L,
-      n_burnin = 5000L#,
-      # priors = list(
-      #   # Local ridge priors for each alpha ~ N(0, 1/2) - change 3
-      #   Tau_alphas = lapply(seq_len(3), function(alpha) matrix(data = 1)) # change
-      # )
-    )
-  ),
-  tar_target(
-    JM_CD8_allDLI_nointer,
-    run_jointModel(
-      long_obj = long_submodels$CD8_abs_log,
-      surv_obj = cox_all_dli,
-      fform = ~ trans1 + trans2 + trans3 - 1
-    )
-  ),
-  tar_target(
-    JMbayes2_CD8_allDLI_nointer, # name is non case sensitive!
-    jm(
-      Surv_object = cox_all_dli,
-      Mixed_objects = long_submodels$CD8_abs_log,
-      time_var = "intSCT2_5",
-      functional_forms = list(
-        "CD8_abs_log" = ~ value(CD8_abs_log):(trans1 + trans2 + trans3 - 1)
-      ),
-      data_Surv = dli_msdata, # try with this
       n_iter = 15000L,
       n_burnin = 5000L#,
       # priors = list(
@@ -175,73 +139,7 @@ list(
       n_burnin = 5000L
     )
   ),
-  tar_target(
-    multivar_allDLI_nointer_penal, # name is non case sensitive!
-    jm(
-      Surv_object = cox_all_dli,
-      Mixed_objects = long_submodels,
-      time_var = "intSCT2_5",
-      functional_forms = list(
-        "CD4_abs_log" = ~ value(CD4_abs_log):(trans1 + trans2 + trans3 - 1),
-        "CD8_abs_log" = ~ value(CD8_abs_log):(trans1 + trans2 + trans3 - 1)
-      ),
-      data_Surv = dli_msdata, # try with this
-      n_iter = 15000L,
-      n_burnin = 5000L,
-      priors = list(
-        # Local ridge priors for each alpha ~ N(0, 1/2) - change 3
-        Tau_alphas = lapply(seq_len(6), function(alpha) matrix(data = 0.5))
-      )
-    )
-  ),
-  tar_target(
-    multivar_allDLI_inter_penal, # name is non case sensitive!
-    jm(
-      Surv_object = cox_all_dli,
-      Mixed_objects = long_submodels,
-      time_var = "intSCT2_5",
-      functional_forms = list(
-        "CD4_abs_log" = ~ value(CD4_abs_log):(
-          trans1 + trans1:DLI.1 + trans2 + trans2:DLI.2 + trans3 + trans3:DLI.3 - 1
-        ),
-        "CD8_abs_log" = ~ value(CD8_abs_log):(
-          trans1 + trans1:DLI.1 + trans2 + trans2:DLI.2 + trans3 + trans3:DLI.3 - 1
-        )
-      ),
-      data_Surv = dli_msdata, # try with this
-      n_iter = 15000L,
-      n_burnin = 5000L,
-      priors = list(
-        # Local ridge priors for each alpha ~ N(0, 1/2) - change 3
-        Tau_alphas = lapply(seq_len(12), function(alpha) matrix(data = 0.5))
-      )
-    )
-  ),
 
-  # Run reduced models
-  tar_target(
-    multivar_DLIgvhd_inter, # name is non case sensitive!
-    jm(
-      Surv_object = cox_dli_gvhd,
-      Mixed_objects = long_submodels,
-      time_var = "intSCT2_5",
-      functional_forms = list(
-        "CD4_abs_log" = ~ value(CD4_abs_log):(
-          trans1 + trans2 + trans2:DLI.2 + trans3 - 1
-        ),
-        "CD8_abs_log" = ~ value(CD8_abs_log):(
-          trans1 + trans2 + trans2:DLI.2 + trans3 - 1
-        )
-      ),
-      data_Surv = dli_msdata, # try with this
-      n_iter = 15000L,
-      n_burnin = 5000L
-      # priors = list(
-      #   # Local ridge priors for each alpha ~ N(0, 1/2) - change 3
-      #   Tau_alphas = lapply(seq_len(12), function(alpha) matrix(data = 0.5))
-      # )
-    )
-  ),
   tar_target(
     multivar_allDLI_slopeGVHD, # name is non case sensitive!
     jm(
